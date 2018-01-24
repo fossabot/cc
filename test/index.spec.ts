45e28068b895a7ccd9cd2604a62526841594c492
blackcater <blackcater2015@gmail.com>
2018-01-24 22:18:48 +0800

    Using jest for unit test

---
+++ test/index.spec.ts
@@ -0,0 +1,13 @@
+import { add } from '@/index'
+
+describe('@index', () => {
+  describe('#add', () => {
+    it('should return 0', () => {
+      expect(add()).toBe(0)
+    })
+    it('should return right result', () => {
+      expect(add(1, 1)).toBe(2)
+      expect(add(1, 2)).toBe(3)
+    })
+  })
+})
